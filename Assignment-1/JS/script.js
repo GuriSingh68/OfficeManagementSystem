a18903191e1139719c8b5adabde44dd6314d1931
GuriSingh68 <141493117+GuriSingh68@users.noreply.github.com>
2024-09-24 17:46:13 -0400

    Login and SignUp

---
+++ Assignment-1/JS/script.js
@@ -0,0 +1,10 @@
+const login =(event) => {
+    event.preventDefault();
+    alert("Login Sucsessful")
+}
+const signUp =(event) => {
+    event.preventDefault();
+    alert("Sign up Sucsessful")
+}
+document.getElementById("login_submit").addEventListener("click",login);
+document.getElementById("create").addEventListener("click",signUp);
