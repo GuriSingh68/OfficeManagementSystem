b24f65702fa4f49865ad2f5949ced34af1c26bf0
GuriSingh68 <141493117+GuriSingh68@users.noreply.github.com>
2024-09-28 14:11:17 -0400

    Resolved bug

--- Assignment_1/JS/members.js
+++ Assignment_1/JS/members.js
@@ -1,10 +1,11 @@
-document.getElementById("memberDetails").addEventListener("click",function() {
+document.getElementById("memberDetails").addEventListener("click",function(event) {
+  event.preventDefault();
     const detailForm=document.getElementById("detailsForm").classList.toggle("hidden")
   });
 
 const addMember=(event) => {
     event.preventDefault();
-    alert("Successful submission");
     document.getElementById("detailsForm").classList.toggle("hidden");
+    alert("Successful submission");
 } 
-document.getElementById("submitDetails").addEventListener("click",addMember());
+document.getElementById("submitDetails").addEventListener("click",addMember);
